3d5268e9cba8c177ecbbd83b5874a25d6c7ca9ca
Christopher Hall II <christopher.s.hall@duke.edu>
2023-12-24 14:23:19 -0500

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-grey-600">
+      <div>
+
+
+        <div className='bg-blue text-white'>hahahahha</div>
+      </div>
+    </main>
+  )
+}
